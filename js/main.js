822f583079262b90a0012f100d76d7a0302b0ae1
Vance Miller <vbmiller@cs.unc.edu>
2015-06-24 22:01:57 -0700

    background slideshow

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-  $('.carousel').carousel({interval: 7000});
+$(document).ready(function () {
 });
+
+var numSlides = 17;
+var slides = [];
+for (var i = 0; i < numSlides; i++) {
+    slides[i] = 'images/backgrounds/bg (' + i + ').jpg';
+}
+
+$.backstretch(slides, {duration: 3333, fade: 666});
