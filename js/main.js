35b0a8e64d0f4e83550211a46014407061667ee6
Vance Miller <vbmiller@cs.unc.edu>
2015-06-24 22:27:31 -0700

    background slideshow

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 });
 
-var numSlides = 17;
+var numSlides = 24;
 var slides = [];
 for (var i = 0; i < numSlides; i++) {
     slides[i] = 'images/backgrounds/bg (' + i + ').jpg';
