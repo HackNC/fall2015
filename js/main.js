55756dcc883571b51b113b627c485c5b2d391114
vancemiller <vancemiller@gmail.com>
2015-06-25 21:26:32 -0400

    unified formatting and linked all pages

--- js/main.js
+++ js/main.js
@@ -1,10 +1,2 @@
 $(document).ready(function () {
 });
-
-var numSlides = 24;
-var slides = [];
-for (var i = 0; i < numSlides; i++) {
-    slides[i] = 'images/backgrounds/bg (' + i + ').jpg';
-}
-
-$.backstretch(slides, {duration: 3333, fade: 666});
