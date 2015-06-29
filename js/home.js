36bb345d0dfac3cd1de6228c5ea9aaf8a3835bd1
Sarah Rust <serust@cs.unc.edu>
2015-06-28 21:58:00 -0700

    homepage invitation

--- js/home.js
+++ js/home.js
@@ -2,3 +2,9 @@ $(document).ready(function () {
 });
 
 $('body').css('margin-top', $('nav').height());
+
+$('header > .jumbotron').css('height', $(window).height());
+
+$(window).on('resize', function() {
+	$('header > .jumbotron').css('height', $(window).height());
+});
