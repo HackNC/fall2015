9c2a59c8209113cf5f31ec8a9ae4440c625b1c22
vancemiller <vancemiller@gmail.com>
2015-06-30 21:09:31 -0400

    support image captions

--- about/js/about.js
+++ about/js/about.js
@@ -1,11 +1,18 @@
 var numSlides = 24;
 var slides = [];
+var captions = [];
 for (var i = 0; i < numSlides; i++) {
-    slides[i] = 'images/bg (' + i + ').jpg';
+	slides[i] = 'images/bg (' + i + ').jpg';
+	captions[i] = 'test_caption: ' + i;
 }
 
 $.backstretch(slides, {duration: 3333, fade: 666});
 
-$('#info-btn').on('click', function() {
-	$('#info').fadeToggle();
+$('#caption-btn').on('click', function() {
+	$('#caption').fadeToggle();
+});
+
+$(window).on('backstretch.show', function(e, instance) {
+  var caption = captions[instance.index];
+  $('#caption').html('<a href="' + slides[instance.index] + '">' + caption +  '</a><div class="ripple-wrapper"></div>');
 });
