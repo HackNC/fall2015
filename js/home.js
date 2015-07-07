6b75131d414f72b099721c9727140dde79b3b377
vancemiller <vancemiller@gmail.com>
2015-07-06 20:16:17 -0400

    make the faqs even

--- js/home.js
+++ js/home.js
@@ -8,3 +8,10 @@ $('header > .jumbotron').css('height', $(window).height());
 $(window).on('resize', function() {
 /* 	$('header > .jumbotron').css('height', $(window).height()); */
 });
+
+var faqHeight = Math.max.apply(null, $('#faq .panel').map(function() {
+	return $(this).height();
+}).get());
+console.log(faqHeight);
+
+$('#faq .panel').css('height', faqHeight);
