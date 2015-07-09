a66057145435097d5c87209c02c7e1a505d0852b
vancemiller <vancemiller@gmail.com>
2015-07-08 21:31:07 -0400

    nav stuff

--- js/home-nav.js
+++ js/home-nav.js
@@ -2,8 +2,34 @@ var navOffset = function () {
   $('#content.container').css('margin-top', $('#nav').height() * 0.25);
 };
 
-navOffset();
+var collapsedMenus = function() {
+	$('#nav button.navbar-toggle').on('click', function() {
+		if ($(this).hasClass('collapsed')) {
+			var navCollapseSize = 322.5;
+			collapseOffset('#nav', navCollapseSize);
+		}
+	});
+	
+	$('#nav a#side-menu').on('click', function() {
+		if ($(this).parent().hasClass('open')) {
+			var menuCollapseSize = 133;
+			collapseOffset('#nav', menuCollapseSize);
+		}
+	});
+};
 
+var collapseOffset = function(selector, size) {
+	var windowHeight = $(window).height();
+	if (($(selector).offset().top - $(window).scrollTop()) > (size)) {
+		var dest = (size < windowHeight) ? size : windowHeight;
+		$('html, body').animate({
+			scrollTop: dest
+		}, 500, 'swing');
+	}
+};
+
+navOffset();
+collapsedMenus();
 $(window).on('resize', function () {
   navOffset();
 });
