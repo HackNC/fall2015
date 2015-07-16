a6a7487e9a4bcc66a50c3c51247a2e3d33432be0
Vance Miller <vbmiller@cs.unc.edu>
2015-07-15 21:27:27 -0700

    header images

--- js/home-header.js
+++ js/home-header.js
@@ -1,9 +1,7 @@
 var fullHeight = function () {
   var height = $(window).height() - $('nav').height();
-  $('.full-height > .jumbotron').css('height', height);
-  var $title = $('header #title');
-  $title.css('top', height / 2);
-  $title.show();
+  var $fullHeighters = $('.full-height > .jumbotron');
+  $fullHeighters.css('height', height);
 };
 
 fullHeight();
