dc2e2f25b6332d405a838170edaa06efdb482abb
vancemiller <vancemiller@gmail.com>
2015-07-15 21:28:29 -0400

    make the website more North Carolia-ey

--- js/home-header.js
+++ js/home-header.js
@@ -1,9 +1,9 @@
-var headerHeight = function () {
+var fullHeight = function () {
   var height = $(window).height() - $('nav').height();
-  $('header > .jumbotron').css('height', height);
+  $('.full-height > .jumbotron').css('height', height);
   var $title = $('header #title');
   $title.css('top', height / 2);
   $title.show();
 };
 
-headerHeight();
+fullHeight();
