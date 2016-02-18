16886eef5b87088313a16e540e26103d512ac718
madipfaff <mpfaff@live.unc.edu>
2016-02-17 19:20:31 -0500

    trying so hard

--- schedule/js/schedule.js
+++ schedule/js/schedule.js
@@ -1,15 +1,12 @@
 var scheduleLoad = function(url) {
 	//get JSON
 	$.getJSON(url, function(data) {
-		var fritable = $("#fritable");
 		var sattable = $("#sattable");
 		var sntable = $("#sntable");
 		for (var i = 0; i < data.events.length; i++) {
 			thisEvent = data.events[i];
 			var table;
-			if (data.events[i].day.toLowerCase() == "friday") {
-				table = fritable;
-			} else if (thisEvent.day.toLowerCase() == "saturday") {
+			if (data.events[i].day.toLowerCase() == "saturday") {
 				table = sattable;
 			} else {
 				table = sntable;
